41d6b545410f589599a0a4fa98f078c87ee1aba3
rovilay <rovilayjnr01@gmail.com>
2019-08-13 21:17:56 +0100

    * setup fonts and colors

--- src/views/App/index.jsx
+++ src/views/App/index.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>upshot</h1>
-      <span>Go Upshotters!!!</span>
-    </div>
+    <Fragment>
+      <div className="App">
+        <div className="leftSide">
+          <h1 className="logo">Upshot</h1>
+          <div className="content">
+            <div className="msg">
+              <p>Making Events</p>
+              <p>Engaging, Fun and Enjoyable</p>
+            </div>
+            <button className="auth-btn">Signup with google</button>
+          </div>
+        </div>
+        <div className="rightSide" />
+      </div>
+    </Fragment>
   );
 }
 
