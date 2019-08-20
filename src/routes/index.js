24114434123f286af90a28cf33a4f0c91b84351b
rovilay <rovilayjnr01@gmail.com>
2019-08-20 16:51:48 +0100

    * create profile view

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import NotFoundPage from '../views/NotFoundPage';
 import AuthPage from '../views/AuthPage';
 import HomePage from '../views/HomePage';
+import ProfilePage from '../views/ProfilePage';
 
 const Routes = () => (
     <Switch>
@@ -16,6 +17,11 @@ const Routes = () => (
             path="/home"
             exact
             component={HomePage}
+        />
+        <Route
+            path="/profile"
+            exact
+            component={ProfilePage}
         />
         <Route component={NotFoundPage} />
     </Switch>
