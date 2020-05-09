7e8a29dd3b780d57803de6cf4b53d0ed6553134d
Stephen Aribaba <stephen.aribaba@gmail.com>
2020-05-09 20:13:21 +0100

    add route handler to dist server (#89)

--- dist/server.js
+++ dist/server.js
@@ -4,5 +4,12 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, '../build')));
 const port = process.env.PORT || 3000;
+
+// handle all react http requests
+app.get('/*', (request, response) => {
+    response.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+
 app.listen(port);
 console.log(`server started ${port}`);
